b64c5925aec1c77a0914585f404207d40709bee3
KennethChong <kennethchong39@gmail.com>
2020-05-05 17:53:41 -0700

    React Project

---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Title of note</h1>
+            <p> Note content</p>
+        </div>
+    )
+};
+
+export default Note;
